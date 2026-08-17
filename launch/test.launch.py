6aa3f87769c68a6f6ae056e0393243353c8f63cf
t-ueshiba <t.ueshiba@aist.go.jp>
2026-08-17 20:59:32 +0900

    Move test programs to subdirectory task_wrappers

--- launch/test.launch.py
+++ launch/test.launch.py
@@ -28,7 +28,7 @@ def declare_launch_arguments(args):
 def launch_setup(context):
     return [
         Node(package='task_wrappers',
-             executable='test_task_server',
+             executable='test_action_server',
              parameters=[
                  {'policy':   LaunchConfiguration('policy'),
                   'grouping': LaunchConfiguration('grouping')}
